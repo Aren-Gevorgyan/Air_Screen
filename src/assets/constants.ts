14bd205568896189ab7536a37e27b75be8629178
Aren-Gevorgyan <areng4518@gmail.com>
2025-03-06 16:40:49 +0400

    Done mobile version

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -2,9 +2,8 @@ export const IMAGE_URL: string = 'https://image.tmdb.org/t/p/w342';
 export const BASE_URL: string = 'https://api.themoviedb.org/3/movie';
 
 export const breakpoints = {
-  320: { slidesPerView: 1 }, // Mobile
-  480: { slidesPerView: 2 }, // Small tablets
-  768: { slidesPerView: 3 }, // Tablets
+  480: { slidesPerView: 1 }, // Small tablets
+  768: { slidesPerView: 2 }, // Tablets
   1024: { slidesPerView: 4 }, // Laptops
   1280: { slidesPerView: 5 }, // Laptops
   1680: { slidesPerView: 6 }, // Desktops
