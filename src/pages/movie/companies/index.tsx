1b3ceaa60ac88f741dad7b515e77fc23b6766f49
Aren-Gevorgyan <areng4518@gmail.com>
2025-03-13 14:45:21 +0400

    Created the Companies com.

---
+++ src/pages/movie/companies/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+import styles from './styles.module.scss';
+import { ProductionCompaniesType } from '@/assets/types';
+
+type Props = {
+    companies: Array<ProductionCompaniesType>
+}
+
+const Companies: FC<Props> = ({ companies }) => {
+    const title = companies.length > 1 ? 'Ընկերություններ' : 'Ընկերություն'
+    return (
+        <div className={styles.container}>
+            <h4>{title}</h4>
+            {companies.map((val) => (
+                <span key={val.id} className={styles.item}>
+                    {val.name} {val.origin_country}
+                </span>
+            ))}
+        </div>
+    )
+}
+
+export default Companies;
--- src/pages/movie/description/index.tsx
+++ src/pages/movie/description/index.tsx
@@ -1,30 +1,37 @@
 import React, { FC } from 'react'
 import styles from './styles.module.scss';
 import { MovieData } from '@/assets/types';
+import Companies from '../companies';
 
 type Props = {
     movie: MovieData,
 }
 
 const Description: FC<Props> = ({ movie }) => {
+    const isLastItem = (i: number) => movie.origin_country.length - 1 === i
+    const country = movie.origin_country.length > 1 ? 'Ծագման երկրները' : 'Ծագման երկիր';
+
     return (
         <div className={styles.container}>
-            <div className={styles.item}>
-                <span>Ծագման երկիր</span>
-                {movie.origin_country.map((val: string) => <span key={val}>{val || ''}</span>)}
-            </div>
-            <div className={styles.item}>
-                <span>Ժամանակը</span>
+            {!!movie.origin_country.length && <div className={styles.item}>
+                <h4>{country}</h4>
+                {movie.origin_country.map((val: string, index: number) =>
+                    <span key={val}>{`${val}${isLastItem(index) ? '' : ','}` || ''}</span>
+                )}
+            </div>}
+            {!!movie.runtime && <div className={styles.item}>
+                <h4>Ժամանակը</h4>
                 <span>{`${movie.runtime}ր.` || ''}</span>
-            </div>
-            <div className={styles.item}>
-                <span>Թողարկման ամսաթիվ</span>
+            </div>}
+            {!!movie.release_date && <div className={styles.item}>
+                <h4>Թողարկման ամսաթիվ</h4>
                 <span>{movie.release_date || ''}</span>
-            </div>
-            <div className={styles.item}>
-                <span>Նկարագրություն</span>
+            </div>}
+            <Companies companies={movie.production_companies} />
+            {!!movie.overview && <div className={styles.item}>
+                <h4>Նկարագրություն</h4>
                 <span>{movie.overview || ''}</span>
-            </div>
+            </div>}
         </div>
     )
 }
