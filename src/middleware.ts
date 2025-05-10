d9b4fc26085ce2d1ef65a6f4b3120231ca38e9a4
Aren-Gevorgyan <areng4518@gmail.com>
2025-05-10 12:06:54 +0400

    Refactor custom middleware to enhance error handling and maintain structure

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,43 +9,47 @@ const customMiddleware = async (
   request: NextRequest,
   auth: ClerkMiddlewareAuth
 ) => {
-  const { userId } = await auth(); // Wait for Clerk authentication
-  const url = request.nextUrl.clone();
-  const genre = url.searchParams.get('genre');
-  // const isAuth = url.searchParams.get('auth');
-  const filterValue = url.searchParams.get('value');
-  const locale = url.pathname.split('/')[1];
+  try {
+    const { userId } = await auth(); // Wait for Clerk authentication
+    const url = request.nextUrl.clone();
+    const genre = url.searchParams.get('genre');
+    // const isAuth = url.searchParams.get('auth');
+    const filterValue = url.searchParams.get('value');
+    const locale = url.pathname.split('/')[1];
 
-  // const protectedRoutes = [`/${locale}/my_orders`, `/${locale}/saved`, `/${locale}/order`];
-  const protectedRoutes = [`/${locale}/order`];
-  const isCoincide = protectedRoutes.some((route) =>
-    url.pathname.startsWith(route)
-  );
+    // const protectedRoutes = [`/${locale}/my_orders`, `/${locale}/saved`, `/${locale}/order`];
+    const protectedRoutes = [`/${locale}/order`];
+    const isCoincide = protectedRoutes.some((route) =>
+      url.pathname.startsWith(route)
+    );
 
-  // Redirect if user is not authenticated on protected routes
-  if (isCoincide && !userId) {
-    // if (!isAuth) {
-    //   url.searchParams.set('auth', 'false');
-    //   const redirect = url.href.includes('my_orders') ? url : new URL('/', request.url);
-    return NextResponse.redirect(new URL('/', request.url));
-    // }
+    // Redirect if user is not authenticated on protected routes
+    if (isCoincide && !userId) {
+      // if (!isAuth) {
+      //   url.searchParams.set('auth', 'false');
+      //   const redirect = url.href.includes('my_orders') ? url : new URL('/', request.url);
+      return NextResponse.redirect(new URL('/', request.url));
+      // }
+    }
+
+    // Ensure default genre is set
+    if (url.pathname === `/${locale}` && !genre) {
+      url.searchParams.set('genre', ACTION_GENRE_ID);
+      return NextResponse.redirect(url);
+    }
+
+    // Redirect to home if no filter value in search
+    if (url.pathname === `/${locale}/search` && !filterValue) {
+      url.pathname = '/';
+      url.href = `${request.nextUrl.origin}${url.pathname}`;
+      return NextResponse.redirect(url);
+    }
+
+    // Return null to let further middleware proceed if no redirects are required
+    return null;
+  } catch (error) {
+    console.log("🚀 ~ error:", error)
   }
-
-  // Ensure default genre is set
-  if (url.pathname === `/${locale}` && !genre) {
-    url.searchParams.set('genre', ACTION_GENRE_ID);
-    return NextResponse.redirect(url);
-  }
-
-  // Redirect to home if no filter value in search
-  if (url.pathname === `/${locale}/search` && !filterValue) {
-    url.pathname = '/';
-    url.href = `${request.nextUrl.origin}${url.pathname}`;
-    return NextResponse.redirect(url);
-  }
-
-  // Return null to let further middleware proceed if no redirects are required
-  return null;
 };
 
 // Combine Clerk Middleware with Custom Logic
